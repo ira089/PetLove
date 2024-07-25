9fddb3f791ae9f4beb968c6964760ea11afe1e80
ira089 <gvir2015@gmail.com>
2024-07-25 22:42:50 +0300

    formAnt

--- src/Pages/Layout/Layout.jsx
+++ src/Pages/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Button } from 'antd';
 import styles from './layout.module.css';
+import RegisterForm from 'components/RegisterForm/RegisterForm';
 
 const Layout = () => {
   return (
-    <div>Layout
+    <div>
+      <RegisterForm/>
        <Button className={styles.wrap} type="primary">lau</Button>
     </div>
     // classNames
