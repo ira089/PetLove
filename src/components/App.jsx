b034651326e2ff759f5cf7dcc03f0c4dc9ca5ddb
ira089 <gvir2015@gmail.com>
2024-07-25 17:53:41 +0300

    App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Routes, Route } from 'react-router-dom';
+// import { lazy } from 'react';
+import Layout from '../Pages/Layout/Layout';
+import PrivateRoute from './PrivateRoute/PrivateRoute';
+import PublicRoute from './PublicRoute/PublicRoute'
+import Register from 'Pages/Register/Register';
+import Login from 'Pages/Login/Login';
+import NotFoundPage from 'Pages/NotFoundPage/NotFoundPage';
+
+const App = () => {
+  return(
+    <Routes>
+       <Route path="/" element={<Layout />}>
+          <Route element={<PublicRoute />}>
+             <Route path="register" element={<Register />} />
+             <Route path="login" element={<Login />} />
+          </Route>
+          <Route element={<PrivateRoute />}>
+            {/* <Route path="contacts" element={<ContactsPage />} /> */}
+          </Route>
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+    </Routes>
+
+  )
+}
+
+
+
+export default App
+
+
