2e18d73faf606c4ccc295217e71f8fb81b19914b
ira089 <gvir2015@gmail.com>
2024-07-26 00:23:54 +0300

    test

--- src/Pages/Layout/Layout.jsx
+++ src/Pages/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Button } from 'antd';
 import styles from './layout.module.css';
-import RegisterForm from 'components/RegisterForm/RegisterForm';
+// import RegisterForm from 'components/RegisterForm/RegisterForm';
+import Login from 'Pages/Login/Login';
 
 const Layout = () => {
   return (
     <div>
-      <RegisterForm/>
+      {/* <RegisterForm/> */}
+      <Login/>
        <Button className={styles.wrap} type="primary">lau</Button>
     </div>
     // classNames
