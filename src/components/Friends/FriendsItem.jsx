8e6561ef7af9987fb2a686aab54d33a016570243
ira089 <gvir2015@gmail.com>
2024-08-02 23:36:42 +0300

    Update FriendsItem.jsx

--- src/components/Friends/FriendsItem.jsx
+++ src/components/Friends/FriendsItem.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
 
-const FriendsItem = ({item}) => {
-    console.log(item)
+const FriendsItem = ({ item }) => {
+  const { workDays, imageUrl, title, email, phone, address } = item;
+  console.log(workDays);
+  console.log(item);
   return (
-    <>
-     <div>{item.title}</div>
-     <img src={item.imageUrl} alt='logo'/>
-    </>
-   
-  )
-}
+    <li>
+      <div>
+        <p>11:00</p>
+        <p>16:00</p>
+      </div>
+      <img src={imageUrl} alt="logo" />
+      <div>
+        <h3>{title}</h3>
+        <div>
+          <p>
+            <span>Email</span>
+            <a href='#'>{email}</a>
+          </p>
+          <p>
+            <span>Address</span>
+            <a href='#'>{address}</a>
+          </p>
+          <p>
+            <span>Phone</span>
+            <a href='#'>{phone}</a>
+          </p>
+        </div>
+      </div>
+    </li>
+  );
+};
 
-export default FriendsItem
+export default FriendsItem;
