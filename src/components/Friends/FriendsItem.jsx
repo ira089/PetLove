6b83b9a4e6bc7f4f21b9a16b48bb05db97327c45
ira089 <gvir2015@gmail.com>
2024-08-03 14:57:25 +0300

    friend

--- src/components/Friends/FriendsItem.jsx
+++ src/components/Friends/FriendsItem.jsx
@@ -1,32 +1,45 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import styles from './friendsItem.module.css';
 
 const FriendsItem = ({ item }) => {
   const { workDays, imageUrl, title, email, phone, address } = item;
-  console.log(workDays);
+  // console.log(workDays);
   console.log(item);
+
+  // const qwe =
+  //   workDays && workDays.length ? workDays[0].from || workDays[5].from : 0;
+  // console.log(qwe);
+
   return (
-    <li>
-      <div>
-        <p>11:00</p>
-        <p>16:00</p>
+    <li className={styles.itemFriend}>
+      <div className={styles.time}>
+        {workDays && workDays.length ? (
+          <span>{workDays[0].from || workDays[5].from}</span>
+        ) : (
+          <span>Day and night</span>
+        )}
+        {workDays && workDays.length && (
+          <span>{workDays[0].to || workDays[5].to}</span>
+        )}
       </div>
-      <img src={imageUrl} alt="logo" />
       <div>
-        <h3>{title}</h3>
+        <img className={styles.img} src={imageUrl} alt="logo" />
         <div>
-          <p>
-            <span>Email</span>
-            <a href='#'>{email}</a>
-          </p>
-          <p>
-            <span>Address</span>
-            <a href='#'>{address}</a>
-          </p>
-          <p>
-            <span>Phone</span>
-            <a href='#'>{phone}</a>
-          </p>
+          <h3 className={styles.title}>{title}</h3>
+          <div className={styles.text}>
+            <p>
+              <span>Email</span>
+              <a href="#">{email}</a>
+            </p>
+            <p>
+              <span>Address</span>
+              <a href="#">{address}</a>
+            </p>
+            <p>
+              <span>Phone</span>
+              <a href="#">{phone}</a>
+            </p>
+          </div>
         </div>
       </div>
     </li>
