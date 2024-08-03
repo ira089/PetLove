a218aab378d4fc8059ba4f8eacea5882f58bd230
ira089 <gvir2015@gmail.com>
2024-08-03 17:39:39 +0300

    friendItem

--- src/components/Friends/FriendsItem.jsx
+++ src/components/Friends/FriendsItem.jsx
@@ -1,44 +1,48 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import styles from './friendsItem.module.css';
+import Typography from '@mui/material/Typography';
 
 const FriendsItem = ({ item }) => {
   const { workDays, imageUrl, title, email, phone, address } = item;
   // console.log(workDays);
   console.log(item);
-
+  const isLength = Boolean(workDays?.length);
+  const isAddress = Boolean(address);
+  const isPhone = Boolean(phone);
   // const qwe =
   //   workDays && workDays.length ? workDays[0].from || workDays[5].from : 0;
-  // console.log(qwe);
+  // console.log(isLength);
 
   return (
     <li className={styles.itemFriend}>
       <div className={styles.time}>
-        {workDays && workDays.length ? (
+        {isLength ? (
           <span>{workDays[0].from || workDays[5].from}</span>
         ) : (
           <span>Day and night</span>
         )}
-        {workDays && workDays.length && (
-          <span>{workDays[0].to || workDays[5].to}</span>
-        )}
+        {isLength && <span> - {workDays[0].to || workDays[5].to}</span>}
       </div>
-      <div>
+
+      <div className={styles.textImgWrap}>
         <img className={styles.img} src={imageUrl} alt="logo" />
         <div>
           <h3 className={styles.title}>{title}</h3>
           <div className={styles.text}>
-            <p>
-              <span>Email</span>
+            <Typography noWrap>
+              <span>Email: </span>
               <a href="#">{email}</a>
-            </p>
-            <p>
-              <span>Address</span>
-              <a href="#">{address}</a>
-            </p>
-            <p>
-              <span>Phone</span>
-              <a href="#">{phone}</a>
-            </p>
+              
+            </Typography>
+            <Typography noWrap>
+              <span>Address: </span>
+              {isAddress ? <a href="#">{address}</a> : 'website only'}
+            </Typography>
+            <Typography noWrap>
+              <span>Phone: </span>
+              {isPhone ? <a href="#">{phone}</a> : 'email only'}
+            </Typography>
           </div>
         </div>
       </div>
